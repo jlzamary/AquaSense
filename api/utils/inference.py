91aea7706cf30589870a8a9d9d31b356f5f7fa6b
jlzamary <jlzamary@wm.edu>
2025-10-17 18:21:14 -0400

    Update inference.py

--- api/utils/inference.py
+++ api/utils/inference.py
@@ -23,12 +23,8 @@ def load_model(checkpoint_path):
 
 species_classes = ['Crab', 'Eel', 'Flatfish', 'Roundfish', 'Scallop', 'Skate', 'Whelk']
 
-transform = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-])
+weights = models.ResNet50_Weights.DEFAULT  
+transform = weights.transforms()
 
 def predict_species(model, image_bytes, filename):
     image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
